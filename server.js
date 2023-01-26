e5628439aeca612e8a445079dd1d45f8190d8fc2
amirsharifico <amirsharifico@gmail.com>
2023-01-26 09:36:50 -0500

    change password future added

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("./model/user");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const JWT_SECRET=process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
 
 mongoose.connect("mongodb://0.0.0.0:27017/login-app-db", {
   useNewUrlParser: true,
@@ -19,31 +19,8 @@ const app = express();
 app.use("/", express.static(path.join(__dirname, "static")));
 app.use(bodyParser.json());
 
-app.post('/api/login', async(req, res)=>{
-    const {username, password}= req.body
-    const user = await User.findOne({username}).lean()
-    if(!user){
-        return res.json({status:'error', error:'Invalid username/password'})
-    }
-    if(await bcrypt.compare(password, user.password)){
-        const token = jwt.sign({
-            id: user._id, 
-            username: user.username
-        }, 
-        JWT_SECRET)
-        return res.json({status:'ok', data:token})
-    }
-    res.json({status:'error', error:'Invalid username/password'})
-})
-
-app.post("/api/register", async (req, res) => {
-
-
-  const { username, password: plainTextPassword } = req.body;
-
-  if(!username || typeof username !== 'string'){
-    return res.json({status:'error', error: 'Invalid Username'})
-  }
+app.post("api/change-password", async (req, res) => {
+  const { token, newpassword:plainTextPassword } = req.body;
 
   if(!plainTextPassword || typeof plainTextPassword !== 'string'){
     return res.json({status:'error', error: 'Invalid password'})
@@ -53,7 +30,61 @@ app.post("/api/register", async (req, res) => {
     return res.json({status:'error', error: 'Password must be at least 6 characters'})
   }
 
-  
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    const _id = user.id;
+    const password = await bcrypt.hash(plainTextPassword, 10);
+    await User.updateOne(
+      { _id },
+      {
+        $set: { password },
+      }
+    )
+    res.json({status:'ok'})
+  } catch (error) {
+    res.json({ status: "error", error: ";))" });
+  }
+
+
+});
+
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).lean();
+  if (!user) {
+    return res.json({ status: "error", error: "Invalid username/password" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      JWT_SECRET
+    );
+    return res.json({ status: "ok", data: token });
+  }
+  res.json({ status: "error", error: "Invalid username/password" });
+});
+
+app.post("/api/register", async (req, res) => {
+  const { username, password: plainTextPassword } = req.body;
+
+  if (!username || typeof username !== "string") {
+    return res.json({ status: "error", error: "Invalid Username" });
+  }
+
+  if (!plainTextPassword || typeof plainTextPassword !== "string") {
+    return res.json({ status: "error", error: "Invalid password" });
+  }
+
+  if (plainTextPassword.length < 5) {
+    return res.json({
+      status: "error",
+      error: "Password must be at least 6 characters",
+    });
+  }
+
   const password = await bcrypt.hash(plainTextPassword, 10);
 
   try {
@@ -66,7 +97,7 @@ app.post("/api/register", async (req, res) => {
     if (error.code === 11000) {
       return res.json({ status: "error", error: "Username already exist " });
     }
-    throw error
+    throw error;
   }
 
   res.json({ status: "ok" });
